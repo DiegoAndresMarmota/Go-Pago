868c458e372cb144de7fe5081c481771eb65b6f8
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-13 15:53:46 -0400

    Creación de cardModel

---
+++ api/models/cardModel.go
@@ -0,0 +1,51 @@
+package model
+
+type Card struct {
+	Properties bool
+}
+
+type Properties struct {
+	Id                string `json:"id"`
+	Token             string `json:"token"`
+	userID            string `json:"customer_id"`
+	ExpirationMonth   int    `json:"expiration_month"`
+	ExpirationYear    int    `json:"expiration_year"`
+	FirstSixDigits    string `json:"first_six_digits"`
+	LastFourDigits    string `json:"last_four_digits"`
+	PaymentProperties PaymentProperties
+	SecurityCode      SecurityCode
+	Issuer            Issuer
+	Cardholder        Cardholder
+	DateCreated       string `json:"date_created"`
+	DateLastUpdated   string `json:"date_last_updated"`
+	PaymentMethodId   string `json:"payment_method_id"`
+}
+
+type PaymentProperties struct {
+	Id              string `json:"id"`
+	Name            string `json:"name"`
+	PaymentTypeId   string `json:"payment_type_id"`
+	Thumbnail       string `json:"thumbnail"`
+	SecureThumbnail string `json:"secure_thumbnail"`
+}
+
+type SecurityCode struct {
+	Length       int    `json:"length"`
+	CardLocation string `json:"card_location"`
+}
+
+type Issuer struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type Cardholder struct {
+	Name           string `json:"name"`
+	Identification IdentificationHolder
+}
+
+type IdentificationHolder struct {
+	Number  string `json:"number"`
+	SubType string `json:"sub_type"`
+	Type    string `json:"type"`
+}
