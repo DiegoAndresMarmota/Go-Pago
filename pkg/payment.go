96016ff11ab4d62e67753c3c6fcd302157b5ff46
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-14 13:21:26 -0400

    Creación de PaymentModel

---
+++ pkg/payment.go
@@ -0,0 +1,29 @@
+package pkg
+
+import ()
+
+// Payment proporciona acceso a la API para crear pagos.
+type Payment struct {
+	Schema	paymentModel
+}
+
+// Create crea un pago
+func (p *Payment) Create() {
+	
+}
+
+// Update actualiza un pago
+func (p *Payment) Update() {
+	
+}
+
+// Get obtiene un pago por ID
+func (p *Payment) Get() {
+	
+}
+
+// Search busca pagos en la base de datos
+func (p *Payment) Search() {
+	
+}
+
