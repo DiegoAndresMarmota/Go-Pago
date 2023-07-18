23033a19c8bb96c6be7428f00978d2e7eae1f674
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-18 18:20:27 -0400

    Creación de func MoneyValidate ( )

---
+++ pkg/moneyValidate.go
@@ -0,0 +1,25 @@
+package pkg
+
+import (
+	"github.com/DiegoAndresMarmota/Go-Pago/api/models"
+)
+
+type MoneyValidate struct {
+	Schema models.MoneyValidate
+}
+
+func (mv *MoneyValidate) Create() {
+
+}
+
+func (mv *MoneyValidate) Save() {
+
+}
+
+func (mv *MoneyValidate) GetAll() {
+
+}
+
+func (mv *MoneyValidate) FindById() {
+
+}
