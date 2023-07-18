f8f1c4f393db8b26beca24395c7eb44f1a9bdf1d
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-07-18 18:21:26 -0400

    Features/pkg 01 (#10)
    * Creación de func Configuration ( )
    
    * Creación de func MoneyValidate ( )
    
    * Creación de func RefundMoney( )

---
+++ pkg/configuration.go
@@ -0,0 +1,28 @@
+package pkg
+
+import "github.com/DiegoAndresMarmota/Go-Pago/api/models"
+
+type Configuration struct {
+	Schema models.Configuration
+	PartnersHeaders bool
+}
+
+func (co *Configuration) Create() {
+
+}
+
+func (co *Configuration) Save() { 
+
+}
+
+func (co *Configuration) Update() {
+
+}
+
+func (co *Configuration) GetAll() {
+
+}
+
+func (co *Configuration) FindById() {
+
+}
---
+++ pkg/refundMoney.go
@@ -0,0 +1,20 @@
+package pkg
+
+import "github.com/DiegoAndresMarmota/Go-Pago/api/models"
+
+type RefundMoney struct {
+	Schema models.RefundMoney
+	Idempotency bool
+}
+
+func (rf *RefundMoney) GetAll() {
+
+}
+
+func (rf *RefundMoney) Create() {
+
+}
+
+func (rf *RefundMoney) Save() {
+
+}
