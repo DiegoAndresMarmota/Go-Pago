0926e641a01e83ac0231c5a1c88d2648ae7f6d21
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-08-01 15:01:58 -0400

    Features/config 02 (#17)
    * Creación de GoPagoResponse
    
    * Modificación de imports
    
    * Modificación de configurations
    
    * Creación de GoPagoError

--- config/config.go
+++ config/config.go
@@ -1,18 +1,117 @@
 package config
 
-import ()
+import (
+	"errors"
+)
 
-type Headers map[string] interface{}
+var (
+	clientId, clientSecret, platformId, corporationId, integratorId, accessToken, refreshToken string
+	)
+var productId = []string{""}
+var schema = "https"
+var host = "api.gopago.com"
+var userAgent = []string{"GoPago"}
 
-type Settings struct {
-	URL          string
-	Method       string
-	Headers		 string
-	Data         interface{}
+type Configurations struct {
+	Sandbox           	bool
+	ShowPromiseError  	bool
+	CacheMaxSize      	uint16 //CacheMaxSize = 50
+	ClientID 			uint16
+	ClientSecret 		uint16
+	AccessToken 		string
+	ProductId 			string
+	TrackingId 			string
+	UserAgent 			string
 }
 
-type Response struct {
-	StatusCode int
-	Headers    Headers
-	Body       map[string]interface{}
+//Añadir ClientID, ClientSecret, AccessToken y ... en ruta admin
+
+func Configure(configs Configurations) {
+	if configs.ClientID == "" && configs.ClientSecret == "" && configs.AccessToken == "" {
+		panic(errors.New("You must provide a method of authentication (ClientId & ClientSecret or AccessToken)"))
+	}
+	if (configs.ClientId != "" && configs.ClientSecret == "") ||
+		(configs.ClientId == "" && configs.ClientSecret != "") {
+		panic(errors.New("You must provide both ClientId and ClientSecret"))
+	}
+
+	clientId = configs.ClientId
+	clientSecret = configs.ClientSecret
+
+	accessToken = configs.AccessToken
+	platformId = configs.PlatformId
+	corporationId = configs.CorporationId
+	integratorId = configs.IntegratorId
+
+	Configurations.Sandbox = configs.Sandbox
+	Configurations.ShowPromiseError = configs.ShowPromiseError
+
+	if !Configurations.ShowPromiseError {
+	
+	}
+}
+
+
+func GetClientId() string {
+	return clientId
+}
+
+
+func GetClientSecret() string {
+	return clientSecret
+}
+
+
+func GetPlatformId() string {
+	return platformId
+}
+
+
+func GetCorporationId() string {
+	return corporationId
+}
+
+
+func GetIntegratorId() string {
+	return integratorId
+}
+
+
+func SetAccessToken(token string) {
+	accessToken = token
+}
+
+
+func GetAccessToken() string {
+	return accessToken
+}
+
+
+func SetRefreshToken(token string) {
+	refreshToken = token
+}
+
+
+func GetRefreshToken() string {
+	return refreshToken
+}
+
+
+func GetBaseUrl() string {
+	return schema + "://" + host
+}
+
+
+func GetProductId() string {
+	return ProductId
+}
+
+
+func GetTrackingId() string {
+	return TrackingID
+}
+
+
+func GetUserAgent() string {
+	return userAgent
 }
