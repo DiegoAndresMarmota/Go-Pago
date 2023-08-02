31d5f0ba0b721bc1fcdf59ad70e34c9adfe7749c
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-08-02 13:26:42 -0400

    Features/config 03 (#19)
    * Creación de interface NewGoPagoDate
    
    * Creación de gpd ToString.Format
    
    * Creación de GetDate( ) en GoPagoDate

--- config/goPagoDate.go
+++ config/goPagoDate.go
@@ -21,10 +21,31 @@ func NewGoPagoDate(args ...interface{}) GoPagoDate {
 	}
 }
 
+
 func (gpd GoPagoDate) ToString(utc uint8) string {
 	if utc != 0 {
 		utcOff := time.FixedZone("UTC", int(utc))
 		return gpd.DatePurchase.In(utcOff).Format("2023-01-02 13:13:13")
 	}
 	return gpd.DatePurchase.Format("2023-01-02 13:13:13")
+}
+
+
+//GetDate convierte GoPagoDate en un objeto de DatePurchase
+func (gpd *GoPagoDate) GetDate() time.Time {
+	return gpd.DatePurchase
+}
+
+
+//AddPurchase agrega dias a la fecha actual de la compra
+func (gpd *GoPagoDate) AddPurchase(days int) *GoPagoDate{
+	gpd.DatePurchase = gpd.DatePurchase.AddDate(0, 0, days)
+	return gpd
+}
+
+
+//RemovePurchase resta dias a la fecha actual de la compra
+func (gpd *GoPagoDate) RemovePurchase(days int) *GoPagoDate{
+	gpd.DatePurchase = gpd.DatePurchase.AddDate(0, 0, -days)
+	return gpd
 }
